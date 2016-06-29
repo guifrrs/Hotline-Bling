5d84a41719d77e0523cdd7ebb85cf9e57b281b08
e-kohler <eduardo.kohler98@gmail.com>
2016-06-29 17:57:58 -0300

    Update

--- conta_desc.vhd
+++ conta_desc.vhd
@@ -1,28 +1,28 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity Conta_DES is
 	port (CLK2, RST, EN: in std_logic;
-			Conta_Des: out std_logic_vector(9 downto 0);
+			Conta_Des: out std_logic_vector(9 downto 0)
 			);
 end Conta_DES;
 
 architecture bhv_conta of Conta_DES is
-signal desc: integer;
-signal desc2: integer;
+
+	signal temp : std_logic_vector(9 downto 0);
+
 	begin
 		process(CLK2, RST, EN)
 			begin
-				desc <= 1010;
-				if RST = '0' then
-					Conta_Des <= (others => '0');
-				elsif	rising_edge(CLK2) then
+				if RST = '0' then 
+					temp <= "1111110010"; 
+				elsif rising_edge(CLK2) then
 					if EN = '1' then
-						Conta_DES <= desc;
-					if desc /= 0 then
-						desc2 <= desc - 1;
-						desc <= desc2;
-					end if;	
+						if temp /= '0' then
+						temp <= temp - 1;
+						Conta_Des <= temp;
+						end if;
 					end if;
 				end if;
 			end process;
