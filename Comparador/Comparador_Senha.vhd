c243c23e73d626afededaaa7f0758c7cdaadaf11
e-kohler <eduardo.kohler98@gmail.com>
2016-07-06 17:49:34 -0300

    Update Comparador_Senha

--- Comparador/Comparador_Senha.vhd
+++ Comparador/Comparador_Senha.vhd
@@ -3,22 +3,19 @@ use IEEE.std_logic_1164.all;
 
 entity Comparador_Senha is
 	port	(SEQ_3, SEQ_2, SEQ_1, SEQ_0: in std_logic_vector(4 downto 0);
-				Password: in std_logic_vector(19 downto 0);
-				Saida: out std_logic
-				);
+		Saida: out std_logic
+		);
 end Comparador_Senha;
 
 architecture bhv_comp of Comparador_Senha is
-
-	signal S: std_logic;
-	signal Pass: std_logic_vector(19 downto 0) := "00000000000000000000";
+	signal Pass: std_logic_vector(19 downto 0) := "00000001110001000010";
 	begin
 		process(SEQ_3, SEQ_2, SEQ_1, SEQ_0)
 			begin
-				if(SEQ_3 = Password(19 downto 15)) then
-					if(SEQ_2 = Password(14 downto 10)) then
-						if(SEQ_1 = Password(9 downto 5)) then
-							if(SEQ_0 = Password(4 downto 0)) then
+				if(SEQ_3 = Pass(19 downto 15)) then
+					if(SEQ_2 = Pass(14 downto 10)) then
+						if(SEQ_1 = Pass(9 downto 5)) then
+							if(SEQ_0 = Pass(4 downto 0)) then
 								Saida <= '1';
 							else
 								Saida <= '0';
