1e9a2277ef6a8550dbd707366e4d128dfcafc29b
Guilherme Silva <antonioguil@gmail.com>
2016-07-07 02:33:31 -0300

    Aaa

--- Registrador/Topo_Registrador.vhd
+++ Registrador/Topo_Registrador.vhd
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 
 entity Topo_Registrador is
   port (BTN0, C0, C1, C2, C3: in std_logic; -- Reset e enable
-        CLOCK_50: in std_logic;
+        Clock_50: in std_logic; -- Clock
         REG: in std_logic_vector(19 downto 0);  -- Entrada da senha
         SEQ_3, SEQ_2, SEQ_1, SEQ_0: out std_logic_vector(4 downto 0) -- Saida da senha
         );
