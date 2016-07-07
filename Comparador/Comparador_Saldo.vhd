92b117150919ba8f050e0e4643ac67ff7a3383ca
Guilherme Silva <antonioguil@gmail.com>
2016-07-07 00:17:11 -0300

    Update Comparador_Saldo

--- Comparador/Comparador_Saldo.vhd
+++ Comparador/Comparador_Saldo.vhd
@@ -3,8 +3,8 @@ use IEEE.std_logic_1164.all;
 
 entity Comparador_Saldo is
 	port (Conta_Des: in std_logic_vector(9 downto 0);
-			Saldo: out std_logic
-			);
+	     Saldo: out std_logic
+	     );
 end Comparador_Saldo;
 
 architecture bhv_compSaldo of Comparador_Saldo is
@@ -12,7 +12,6 @@ architecture bhv_compSaldo of Comparador_Saldo is
 	signal zero: std_logic_vector(9 downto 0) := "0000000000";
 
 	begin
-
 		process(Conta_Des)
 			begin
 				if(Conta_Des = zero) then
